7700e7f7593e5330a91655508364b31649e37bf6
Bilal1h <99132773+Bilal1h@users.noreply.github.com>
2023-03-21 17:22:21 +0500

    Add files via upload

---
+++ Alaya Pharma/main.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, send_file
+import http.client
+import ssl 
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return send_file('/home/AlayaPharma/mysite/index.html')
+
+@app.route('/send-message', methods=['GET', 'POST'])
+def send_message():
+    if request.method == 'POST':
+        # Get the form data
+        email = request.form['email']
+        messages = request.form['message']
+        message = f"Email : {email}, Message : {messages}"
+        conn = http.client.HTTPSConnection("api.ultramsg.com",context = ssl._create_unverified_context())
+
+        payload = f"token=csg97w489e4u9zws&to=+923002766638&body={message}"
+        payload = payload.encode('utf8').decode('iso-8859-1') 
+
+        headers = { 'content-type': "application/x-www-form-urlencoded" }
+
+        conn.request("POST", "/instance40688/messages/chat", payload, headers)
+
+        res = conn.getresponse()
+        data = res.read()
+
+        print(data.decode("utf-8"))
+        # TODO: Send the message using your preferred API
+
+        # Return a response
+        return 'Message sent successfully'
+
+    # If the request is a GET request, render the send_message.html template
+
+
+if __name__ == '__main__':
+    app.run()
